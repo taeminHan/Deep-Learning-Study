83b7e66655ea8b013398662862200ea39c53db75
taemin <taemin9705@gmail.com>
2020-08-25 23:39:39 +0900

    NAND, OR

--- Perceptron.py
+++ Perceptron.py
@@ -22,7 +22,7 @@ print(AND(1, 0))  # 0
 0이 나오는 이유는 AND 게이트에서는 두 입력이 모두 1일 경우에만 참, 그 이외에는 0을 반환한다.
 """
 
-#===================================================================================
+# ===================================================================================
 
 
 """
@@ -31,16 +31,19 @@ w1과 w2는 각 입력 신호가 결과에 주는 영향력(중요도)을 조절
 편향은 뉴런이 얼마나 쉽게 활성화하느냐를 조정하는 매개변수이다.
 """
 
+
 # weight and bias in AND gate
 def AND2(x1, x2):
-    x = np.array([x1, x2])  # intput
+    x = np.array([x1, x2])  # 입력
     w = np.array([0.5, 0.5])  # weight (가중치)
     b = -0.7  # bias (민감도)
-    tmp = np.sum(w*x) + b
+    tmp = np.sum(w * x) + b
     if tmp <= 0:
         return 0
     else:
         return 1
+
+
 print(AND2(1, 0.4))
 
 """
@@ -49,4 +52,30 @@ np.sum(w*x) + b
 = (0.5 * 1) + (0.5 * 0.4) - 0.7
 = 0.5 + 0.2 - 0.7
 = 0
+"""
+
+
+def NAND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])  # AND 와는 가중치(w와 b)만 다르다
+    b = 0.7
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])  # AND 와는 가중치(w와 b)만 다르다.
+    b = -0.3
+    tmp = np.sum(w * x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+"""
+AND, NAND, OR는 모두 같은 구조의 퍼셉트론이고, 차이는 가중치 매개변수의 값이다.
 """

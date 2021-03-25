19f6cb230c4b13feb1fd2abc152cfa2130fff6bc
taemin <taemin9705@gmail.com>
2021-03-25 23:56:55 +0900

    2021-03-25

--- 선형모델연습1.py
+++ 선형모델연습1.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import sklearn.linear_model
 
+# 데이터 적재
 oecd_bli = pd.read_csv("oecd_bli_2015.csv", thousands=',')
 gdp_per_capita = pd.read_csv("gdp_per_capita.csv", thousands=',', delimiter='\t', encoding='latin1', na_values="n/a")
 
@@ -12,12 +13,19 @@ country_stats = prepare_country_stats(oecd_bli, gdp_per_capita)
 x = np.c_[country_stats["GDP per capita"]]
 y = np.c_[country_stats["Life satisfaction"]]
 
+# 데이터 시각화
+
 country_stats.plot(kind='scatter', x="GDP per capita", y='Life satisfaction')
 plt.show()
 
+# 선형 모델 선택
+
 model = sklearn.linear_model.LinearRegression()
 
+# 모델 훈련
 model.fit(x, y)
 
+# 키프로스에 대한 예측 만들기
+# 추론이라고 한다.
 X_new = [[22587]]
 print(model.predict(X_new))
